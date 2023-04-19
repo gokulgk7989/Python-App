6f06d311b3aac25004122c95627bf7935edf3294
gokulgk7989 <112245233+gokulgk7989@users.noreply.github.com>
2023-04-19 23:11:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-    return '<h1 style="color:red">Welcome to Demo python app</h1>'
+    return '<h1 style="color:green">Welcome to Demo python app -version-2.0.1</h1>'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
